bce9911c1c839f3421594a65e14a1ee90de9ca80
zszazi <41579863+zszazi@users.noreply.github.com>
2019-05-31 20:34:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ myhelper.imshow(image[0,:]);
 class FashionClassifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(784, 500)
-        self.fc2 = nn.Linear(500, 256)
+        self.fc1 = nn.Linear(784, 512)
+        self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, 128)
         self.fc4 = nn.Linear(128, 64)
         self.fc5 = nn.Linear(64, 10)
